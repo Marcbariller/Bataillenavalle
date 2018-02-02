d0943d2a6db3abbded9040cf3734a4d4b25b7369
Alexis Pannetier <alexis.pannetier@hotmail.fr>
2018-02-02 14:58:04 +0100

    view update

--- src/bataillenavale/Application.java
+++ src/bataillenavale/Application.java
@@ -7,10 +7,10 @@ public class Application {
 		
 		// View Test
 		int x = 12;
-		int y = 4;
+		int y = 10;
 		int[][] map = new int[x][y];
 		View.showMy(map);
-		
+		View.showHis(map);
 	}
 
 }
--- src/bataillenavale/View.java
+++ src/bataillenavale/View.java
@@ -1,28 +1,33 @@
 package bataillenavale;
 
 public class View {
+	// Change symbol border here ->
+	public static String border = "+";
 
 	public static void showMy(int map[][]) {
-		// Change symbol border here
-		String border = "+";
 		int x = 0;
 		int y = 0;
 		for (int i = 0; i < map.length + 2; i++) {
 			System.out.print(border);
 		}
 		System.out.println();
-
 		for (int i = 0; i < map[x].length; i++) {
 			for (int j = 0; j <= map.length + 1; j++) {
-				
 				if (j == 0) {
 					System.out.print(border);
 				} else if (j == map.length + 1) {
 					System.out.print(border);
 				} else {
-					// Ne se deplace pas encore dans la map
 					System.out.print(map[x][y]);
-
+					if (x + 1 == map.length && y == map[x].length) {
+						System.out.println();
+						System.out.println("error: out of array map, on load view");
+					} else if (x + 1 == map.length) {
+						x = 0;
+						y++;
+					} else {
+						x++;
+					}
 				}
 			}
 			System.out.println();
@@ -33,7 +38,45 @@ public class View {
 		System.out.println();
 	}
 
-	public static void showHis(int[][] map) {
+	public static int hiddenShip(int dataMap) {
+		// int result;
+		int result = 0;
+		// changer result selon la valeur des bateaux à cacher, afficher seulement les
+		// bateaux touché/coulé et tire effectué
+		return result;
+	}
 
+	public static void showHis(int[][] map) {
+		int x = 0;
+		int y = 0;
+		for (int i = 0; i < map.length + 2; i++) {
+			System.out.print(border);
+		}
+		System.out.println();
+		for (int i = 0; i < map[x].length; i++) {
+			for (int j = 0; j <= map.length + 1; j++) {
+				if (j == 0) {
+					System.out.print(border);
+				} else if (j == map.length + 1) {
+					System.out.print(border);
+				} else {
+					System.out.print(hiddenShip(map[x][y]));
+					if (x + 1 == map.length && y == map[x].length) {
+						System.out.println();
+						System.out.println("error: out of array map, on load view");
+					} else if (x + 1 == map.length) {
+						x = 0;
+						y++;
+					} else {
+						x++;
+					}
+				}
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < map.length + 2; i++) {
+			System.out.print(border);
+		}
+		System.out.println();
 	}
 }
