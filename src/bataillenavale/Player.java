ffa56e3a5dfa36669b9389ffbe2cbadc8add7def
sylvainbernard29 <dranrebniavlys@gmail.com>
2018-02-02 11:05:39 +0100

    playercreate

--- src/bataillenavale/Player.java
+++ src/bataillenavale/Player.java
@@ -1,5 +1,22 @@
 package bataillenavale;
 
+import java.util.ArrayList;
+
 public class Player {
+	private String name;
+	private ArrayList<Ship> shiplist;
+
+	public String getName() {
+		return name;
+	}
+
+	public ArrayList<Ship> getshiplist() {
+		return shiplist;
+	}
+	
+	public Player(ArrayList<Ship> shiplist){
+		this.shiplist = shiplist;
+	}
 
 }
+
