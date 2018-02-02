daf6f48e4d9b0019eb1d5872b9cbd11bc8214902
sylvainbernard29 <dranrebniavlys@gmail.com>
2018-02-02 12:10:46 +0100

    update

--- src/bataillenavale/Player.java
+++ src/bataillenavale/Player.java
@@ -1,6 +1,11 @@
 package bataillenavale;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import model.Ship;
+import util.Position;
 
 public class Player {
 	private String name;
@@ -18,5 +23,11 @@ public class Player {
 		this.shiplist = shiplist;
 	}
 
+    private List<Position> getShipCellList(Ship[] shipList) {
+        List<Position> cellList = new ArrayList<Position>();
+        for (int i = 0; i < shipList.length; i++) {
+            for (int j = 0; j < shipList[i].getLength(); j++) {
+               
+    }
 }
 
